cb517df87527729ba7957111aa751387928edc28
ICalin <iarucalin4@gmail.com>
2023-11-15 18:36:15 +0100

    databse before fix3

--- app/app/settings.py
+++ app/app/settings.py
@@ -81,9 +81,9 @@ DATABASES = {
         "NAME": os.environ.get("DB_NAME"),
         "USER": os.environ.get("DB_USER"),
         "PASSWORD": os.environ.get("DB_PASS"),
+        "PORT": "5432",
     }
 }
-
 
 
 # Password validation
