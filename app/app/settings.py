b6215a129b65b30dfbd559fe970c57780b8ac718
ICalin <iarucalin4@gmail.com>
2023-11-15 20:19:23 +0100

    database final

--- app/app/settings.py
+++ app/app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-!av(02q+l)%5oq!)wisei(z0&sv2-_ynhyk8%q+o(#ol(7u-k@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0' ]
 
 
 # Application definition
